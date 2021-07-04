2627643f157c363963a06ea9709b1d0a51e2b328
Scott Willrich <swillrich@gmail.com>
2021-07-04 10:35:06 -0500

    added comments and set up .gitignore

--- sensor/index.js
+++ sensor/index.js
@@ -3,14 +3,18 @@ const express = require('express');
 // create an instance of our server application
 const app = express();
 
+// initial route for pulling temperature
 app.get('/temperature', function(req, res) {
     res.send('24 C');
 });
 
+// initial route for pulling humidity
 app.get('/humidity', function(req, res) {
     res.send('48%');
 });
 
+// listen for requests on port 3000
+// http://localhost:3000/humidity or http://localhost:3000/temperature
 app.listen(3000, function() {
     console.log('Server listening on port 3000');
 })
