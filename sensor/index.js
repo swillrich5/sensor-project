6a93cb2c1357dd3860431f2acb8ec311bbe02d6d
Scott Willrich <swillrich@gmail.com>
2021-07-04 08:39:08 -0500

    set up temp and humidity routes

--- sensor/index.js
+++ sensor/index.js
@@ -1,1 +1,16 @@
+const express = require('express');
 
+// create an instance of our server application
+const app = express();
+
+app.get('/temperature', function(req, res) {
+    res.send('24 C');
+});
+
+app.get('/humidity', function(req, res) {
+    res.send('48%');
+});
+
+app.listen(3000, function() {
+    console.log('Server listening on port 3000');
+})
